ad08132cb32af20fd6bf036c1f4f87fb6b161947
jeffyjs <jefferson@karunya.edu.in>
2020-07-01 21:09:13 +0530

    Http headers & Query params Interceptor

---
+++ Headers QueryParams & Interceptors/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AuthInterceoptorService } from './auth-interceptor.service';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule],
+  providers: [{
+    provide : HTTP_INTERCEPTORS, 
+    useClass:AuthInterceoptorService, 
+    multi:true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
